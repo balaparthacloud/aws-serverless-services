cf0d2d03e197cc0d6199fb187e2e591b81543abf
par <balaparthacloud@gmail.com>
2018-07-29 17:54:57 +0800

    aws update function correction

--- carebnb-serverless/aws-serverless-scripts/ffg-care-bnb-user-profile/fn-user-update.js
+++ carebnb-serverless/aws-serverless-scripts/ffg-care-bnb-user-profile/fn-user-update.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Added to handle injection
+const vandium = require( 'vandium' );
 var mysql = require('mysql');
 var config = require('./db-config.json');
 var pool  = mysql.createPool({
@@ -10,29 +12,66 @@ var pool  = mysql.createPool({
     port : 3306,
 });
 
-var debug=false;
-
-module.exports.update =  (event, context, callback) => {
+module.exports.update  =  vandium.generic().handler( (event, context, callback) => {
     //prevent timeout from waiting event loop
     context.callbackWaitsForEmptyEventLoop = false;
 
-    pool.getConnection(function(err, connection) {
-        // Use the connection
-        if (err) {
-            console.log("Error in connection database"+err);
-            return;
-        }
-        if(debug)console.dir(connection);
-        connection.query('SELECT * from user', function (error, results, fields) {
+pool.getConnection(function(err, connection) {
+    // Use the connection
+    if (err) {
+        console.log("Error in connection database"+err);
+        return;
+    }
 
-            // And done with the connection.
+
+    var selectSql = "SELECT * From user where ";
+    selectSql =selectSql + " id = "+connection.escape(event.id);
+
+
+    var insertSql = "INSERT INTO user(id,nric)";
+    insertSql = insertSql + " VALUES("+connection.escape(event.id)+","+connection.escape(event.nric)+")";
+
+
+    var updateSql = "UPDATE user set "
+    updateSql = updateSql + "name = " + connection.escape(event.nric) + " WHERE id = " + connection.escape(event.id);
+
+
+    connection.query(selectSql, function (error, results, fields) {
+
+        // And done with the connection.
+        //connection.release();
+        // Handle error after the release.
+        if (error){
             connection.release();
-            // Handle error after the release.
-            if (error){
-                callback(error);
+            callback(error);
+        }else{
+            if(results && Object.keys(results).length !== 0){
+                connection.query(updateSql, function (error, updateResults, fields) {
+                    // And done with the connection.
+                    connection.release();
+                    // Handle error after the release.
+                    if (error){
+                        callback(error);
+                    }else{
+                        callback(null,updateResults);
+                    }
+
+                });
             }else{
-                callback(null,results);
+                connection.query(insertSql, function (error, insertResults, fields) {
+                    // And done with the connection.
+                    connection.release();
+                    // Handle error after the release.
+                    if (error){
+                        callback(error);
+                    }else{
+                        callback(null,insertResults);
+                    }
+
+                });
             }
-        });
+            //callback(null,results);
+        }
     });
-};
+});
+});
